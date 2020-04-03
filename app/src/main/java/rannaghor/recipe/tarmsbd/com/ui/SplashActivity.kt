1af45d9556d5e2e6b2aea149c4db1a3e439aca26
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-04-03 15:20:37 +0600

    material theme with some required dependency.
    rxjava,rxandroid,lottieanimation,retrofit,jsonconverter

---
+++ app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/SplashActivity.kt
@@ -0,0 +1,22 @@
+package rannaghor.recipe.tarmsbd.com.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.postDelayed
+import rannaghor.recipe.tarmsbd.com.MainActivity
+import rannaghor.recipe.tarmsbd.com.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 7000)
+    }
+}
