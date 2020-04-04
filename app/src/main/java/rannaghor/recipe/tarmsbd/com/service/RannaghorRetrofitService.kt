f0e21bd33964a454f191d7953a243eba7ae62716
Rs Robin <srrobin33@gmail.com>
2020-04-05 03:33:15 +0600

    post req for registration

--- app/src/main/java/rannaghor/recipe/tarmsbd/com/service/RannaghorRetrofitService.kt
+++ app/src/main/java/rannaghor/recipe/tarmsbd/com/service/RannaghorRetrofitService.kt
@@ -4,6 +4,7 @@ import io.reactivex.Flowable
 import io.reactivex.Observable
 import rannaghor.recipe.tarmsbd.com.model.Category
 import rannaghor.recipe.tarmsbd.com.model.Recipe
+import rannaghor.recipe.tarmsbd.com.model.User
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -20,4 +21,14 @@ interface RannaghorRetrofitService {
 
     @get:GET("/api/category.php")
     val category: Observable<List<Category>>
+
+    @POST("/api/users.php")
+    fun userRegistration(
+        @Query("name") name:String,
+        @Query("email") email:String,
+        @Query("password") password:String,
+        @Query("mobile") number:String
+    ): Flowable<List<User>>
+
+
 }
--- app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/authentication/SingUpFragment.kt
+++ app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/authentication/SingUpFragment.kt
@@ -1,24 +1,92 @@
 package rannaghor.recipe.tarmsbd.com.ui.authentication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers.io
 import rannaghor.recipe.tarmsbd.com.R
+import rannaghor.recipe.tarmsbd.com.service.RannaghorRetrofitService
+import rannaghor.recipe.tarmsbd.com.service.RetrofitClient
+import rannaghor.recipe.tarmsbd.com.viewmodel.TAG
+import java.util.logging.Logger
 
 /**
  * A simple [Fragment] subclass.
  */
 class SingUpFragment : Fragment(R.layout.fragment_sing_up) {
 
+    private lateinit var mName:EditText
+    private lateinit var mEmail:EditText
+    private lateinit var mNumber:EditText
+    private lateinit var mPass:EditText
+
+
+
+    private val compositeDisposable = CompositeDisposable()
+    private val retrofit = RetrofitClient.instance
+    private val rannaghorRetrofitService = retrofit.create(RannaghorRetrofitService::class.java)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mName=view.findViewById(R.id.editTextFnameSignup)
+        mEmail=view.findViewById(R.id.editTextEmailsignup)
+        mNumber=view.findViewById(R.id.editTextNumbersignup)
+        mPass=view.findViewById(R.id.editTextPasswordsignup)
 
         view.findViewById<TextView>(R.id.texViewLogin)
             .setOnClickListener {
                 val loginActivity: LoginActivity = activity as LoginActivity
                 loginActivity.startFragment(LoginFragment())
             }
+
+        view.findViewById<MaterialButton>(R.id.buttonSignUp)
+            .setOnClickListener{
+                val name=mName.text.toString()
+                val email=mEmail.text.toString()
+                val number=mNumber.text.toString()
+                val pass=mPass.text.toString()
+                if (name.isEmpty()){
+                    mName.setError("required...")
+                    return@setOnClickListener
+                }else if (email.isEmpty()){
+                    mEmail.setError("required...")
+                    return@setOnClickListener
+                }else if(number.isEmpty()){
+                    mNumber.setError("required...")
+                    return@setOnClickListener
+                }else if(pass.isEmpty()){
+                    mPass.setError("required...")
+                    return@setOnClickListener
+                }else if(pass.length<6){
+                    mPass.setError("at least 6 character")
+                    return@setOnClickListener
+                }
+
+                compositeDisposable.add(
+                rannaghorRetrofitService.userRegistration(name,email,pass,number)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                        {
+                    },this::handleError
+                    )
+
+                )
+
+
+            }
+    }
+    private fun handleError(error: Throwable) {
+        Logger.getLogger(TAG).warning("   Error: ${error.localizedMessage}")
     }
 }
