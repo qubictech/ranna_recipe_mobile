78e3f23c66ab9a63ac3170d8bc86d1a289c68c55
Rs Robin <srrobin33@gmail.com>
2020-04-06 01:24:27 +0600

    Shared prefarences save current user

--- app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/authentication/LoginFragment.kt
+++ app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/authentication/LoginFragment.kt
@@ -1,11 +1,16 @@
 package rannaghor.recipe.tarmsbd.com.ui.authentication
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -17,7 +22,11 @@ import rannaghor.recipe.tarmsbd.com.ui.main.MainActivity
 import rannaghor.recipe.tarmsbd.com.viewmodel.TAG
 import java.util.logging.Logger
 
+
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+
+
 
 
     private lateinit var mEmail: EditText
@@ -27,6 +36,22 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private val compositeDisposable = CompositeDisposable()
     private val retrofit = RetrofitClient.instance
     private val rannaghorRetrofitService = retrofit.create(RannaghorRetrofitService::class.java)
+
+    override fun onStart() {
+        super.onStart()
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        prefs.apply {
+            val isLogin = prefs.getString("login", "no")
+
+            if (isLogin=="yes"){
+                startActivity(Intent(context, MainActivity::class.java))
+            }
+
+        }
+
+
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -58,6 +83,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                             {
                                 Logger.getLogger("Signin Response").warning("$it")
                                 startActivity(Intent(context, MainActivity::class.java))
+
+                                val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+                                val  editor =prefs.edit()
+                                editor.putString("login","yes").apply()
+
                             }, this::handleError
                         )
                 )
--- app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/main/PreferenceFragment.kt
+++ app/src/main/java/rannaghor/recipe/tarmsbd/com/ui/main/PreferenceFragment.kt
@@ -1,10 +1,16 @@
 package rannaghor.recipe.tarmsbd.com.ui.main
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import rannaghor.recipe.tarmsbd.com.R
 
 class PreferenceFragment : Fragment(R.layout.fragment_preference) {
+
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
@@ -14,5 +20,23 @@ class PreferenceFragment : Fragment(R.layout.fragment_preference) {
 //                    putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialButton>(R.id.buttonLogout).setOnClickListener(){
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val  editor =prefs.edit()
+            editor.putString("login","no").apply()
+        }
     }
 }
